bf5df5ed4fc12c4ad8eab55a0eb9165f3d254877
gschian0 <gschiano@me.com>
2021-08-12 14:28:37 +0000

    edited app.js

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ const port = 3000;
 app.get('/', (req, res) => {
 	res.send("hello World!");
 });
+
+app.get('/demo', (req,res) => {
+	res.set('we-be-custom', '4life');
+	res.status(418);
+	res.send('zen-zone');
+});
 	
 app.listen(port, () => console.log(`example app listening on port ${port})!`));
